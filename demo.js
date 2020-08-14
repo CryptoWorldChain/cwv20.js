1f64c4dac2754722b3316efff2ab65048a37a932
Catherinens <Catherinens@protonmail.com>
2020-08-14 20:16:23 +0800

    update rpc

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ var rp = require('request-promise')
 
 var BN=require('bn.js');
 //set testnet network type
-chain.config.server_base='http://43.254.1.176:8000/fbs';
+chain.config.server_base='http://127.0.0.1:8000/fbs';
 chain.config.rpc_provider = rp;
 
 // 3c1ea4aa4974d92e0eabd5d024772af3762720a0  79211e47216f5c13c85650fac839078ad6ae2dc074ca4bd1e7817fbdfe8f6e51
@@ -16,6 +16,7 @@ chain.config.rpc_provider = rp;
 // 测试链默认有钱的 3c1ea4aa4974d92e0eabd5d024772af3762720a0  79211e47216f5c13c85650fac839078ad6ae2dc074ca4bd1e7817fbdfe8f6e51
 
 var kp = chain.KeyPair.genFromPrikey('79211e47216f5c13c85650fac839078ad6ae2dc074ca4bd1e7817fbdfe8f6e51')
+// var kp=chain.KeyPair.genRandomKey()
 var from={
     keypair:kp
 }
@@ -32,22 +33,22 @@ chain.rpc.getBalance(from.keypair.hexAddress).then(function(result){
     // })
 
     // ************************2.createContract************************
-    var cvm = "6080604052600060015534801561001557600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550610174806100656000396000f300608060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680633fa4f2451461005c57806364a4635c14610087578063a74c2bb6146100b4575b600080fd5b34801561006857600080fd5b5061007161010b565b6040518082815260200191505060405180910390f35b34801561009357600080fd5b506100b260048036038101908080359060200190929190505050610111565b005b3480156100c057600080fd5b506100c961011f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b60015481565b806001540160018190555050565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff169050905600a165627a7a72305820e58cd47f3736b6239ce4a0c9b59e0bad69c4420c5fcfab6500226f7198be8d2a0029";
-    args={"data":cvm};
-    chain.rpc.createContract(from,args).then(function(result){
-        console.log("cvm:"+result)
-    }).catch(function(error){
-        console.log("error==="+error);
-    })
-
-    // ************************2.createToken************************
-    // from.keypair.nonce=result.nonce;
-    // args={"tos":["a5c31be225011ee6ecceaf3b9b3696db01a20d6c"], "values":["0"],"name":"AAA","symbol":"AAA","decimals":18,"ext_datas":Buffer.from(JSON.stringify({"c":"2"}),"utf8").toString("hex")} 
-    // chain.rpc.createToken(from,args).then(function(result){
-    //     console.log(result);
+    // var cvm = "6080604052600060015534801561001557600080fd5b50336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550610174806100656000396000f300608060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680633fa4f2451461005c57806364a4635c14610087578063a74c2bb6146100b4575b600080fd5b34801561006857600080fd5b5061007161010b565b6040518082815260200191505060405180910390f35b34801561009357600080fd5b506100b260048036038101908080359060200190929190505050610111565b005b3480156100c057600080fd5b506100c961011f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b60015481565b806001540160018190555050565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff169050905600a165627a7a72305820e58cd47f3736b6239ce4a0c9b59e0bad69c4420c5fcfab6500226f7198be8d2a0029";
+    // args={"data":cvm};
+    // chain.rpc.createContract(from,args).then(function(result){
+    //     console.log("cvm:"+result)
     // }).catch(function(error){
     //     console.log("error==="+error);
     // })
+
+    // ************************2.createToken************************
+    from.keypair.nonce=result.nonce;
+    args={"tos":["a5c31be225011ee6ecceaf3b9b3696db01a20d6c"], "values":["0x10"],"name":"AAA","symbol":"AAA","decimals":18,"ext_datas":Buffer.from(JSON.stringify({"c":"2"}),"utf8").toString("hex")} 
+    chain.rpc.createToken(from,args).then(function(result){
+        console.log(result);
+    }).catch(function(error){
+        console.log("error==="+error);
+    })
 
 }).catch(function (err) {
     console.log(err)
@@ -66,7 +67,7 @@ chain.rpc.getBalance(from.keypair.hexAddress).then(function(result){
 //   System.out.println(str);
 
 
-var hex='706172616d6574657220696e76616c69642c2073656e646572206e6f6e6365206973206c61726765207468616e207472616e73616374696f6e206e6f6e6365';
+var hex='0x657865636572726f723a62616c616e6365206f66207468652073656e646572206973206e6f7420656e6f7567682c746f207072696e7420746f6b656e202c6e6565643a34303936';
 // 0x33b2e3c9fd0803ce7fffe00
 // 0x33b2e3c9fd0803ce7fffe00
 // 0x33b2e3c840f12d59937fe00
@@ -87,7 +88,7 @@ function hex2str(hex) {
 　　}
 　　return resultStr.join("");
 }
-// console.log(hex2str(hex))
+console.log(hex2str(hex))
 
 
-// console.log(BigInt('0x33b2e3c9fd0803ce7fffe00'))
+console.log(BigInt('0x0cee'))
